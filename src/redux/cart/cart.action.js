9852828328e18facfe2daa89ea34aad9696ac6ac
shivam damre <shivamdamre913@gmail.com>
2021-06-03 11:30:12 +0530

    clear cart redux saga added

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -18,3 +18,7 @@ export const decreaseCartItem = item => ({
     type: CartActionTypes.DECREASE_CART_ITEM,
     payload: item
 })
+
+export const clearCart = () => ({
+    type: CartActionTypes.CLEAR_CART
+})
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -27,7 +27,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: decreaseItemFromCart(state.cartItems, action.payload) 
-            }            
+            }           
+        case CartActionTypes.CLEAR_CART:
+            return {
+                ...state,
+                cartItems: []
+            }     
         default:
             return state;    
     }
