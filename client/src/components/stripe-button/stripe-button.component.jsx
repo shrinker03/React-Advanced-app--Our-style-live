8cde353c37e5803f281947f520cac577a548a556
shivam damre <shivamdamre913@gmail.com>
2021-06-07 12:34:26 +0530

    added the backend of the stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51It2gsSFEeFkAbaaXMfDEPH9oGnW1h1iK5IO8kgxxt4N4P11dmBPpe8BGShMeDveoNhTwCgxhSckkH9JREX5qWaz00G3Z4Yu2c';
 
     const onToken = token => {
-        console.log(token)
-        alert("Payment Successful")
-    }
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
+    
 
     return (
         <StripeCheckout 
@@ -16,6 +33,7 @@ const StripeCheckoutButton = ({price}) => {
             name = "CRWN Clothing"
             billingAddress
             shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
             description={`Your Total is $${price}`}
             amount={priceForStripe}
             panelLabel="Pay Now"
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,4 +25,20 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if(error) throw error;
     console.log('Server runnig on port '+ port);
+})
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'INR'
+    }
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if(stripeErr) {
+            res.status(500).send({error: stripeErr});
+        } else {
+            res.status(200).send({success: stripeRes})
+        }
+    })
 })
