c784151a2745fcb4a06989b4044f5f6f4db57eaa
shivam damre <shivamdamre913@gmail.com>
2021-06-18 22:15:46 +0530

    icon component refactored

--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './cart-icon.styles.scss';
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
-import {toggleCartHidden} from '../../redux/cart/cart.action';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
+import {toggleCartHidden} from '../../redux/cart/cart.action';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
