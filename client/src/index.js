8fbbf4f452af7d9b2386e4db707fae0643a1b883
shivam damre <shivamdamre913@gmail.com>
2021-06-21 19:11:06 +0530

    pwa config done

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './redux/store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,4 +19,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
  
-serviceWorker.register();
+serviceWorkerRegistration.register();
