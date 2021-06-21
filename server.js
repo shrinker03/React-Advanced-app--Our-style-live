81b79fb56982b83a80d8d69296206857ef8cc50a
shivam damre <shivamdamre913@gmail.com>
2021-06-21 12:41:35 +0530

    server config added

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
+    app.use(enforce.HTTPS({trustProtoHeader: true}));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
@@ -32,9 +33,9 @@ app.listen(port, error => {
     console.log('Server running on port ' + port);
 });
 
-// app.get('/service-worker.js', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-// })
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+})
 
 app.post('/payment', (req, res) => {
     const body = {
